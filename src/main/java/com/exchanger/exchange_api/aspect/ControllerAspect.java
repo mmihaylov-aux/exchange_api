f4acfad0e5811ef6dd5111a632bf9df65d041d0e
mmihaylov-aux <71435826+mmihaylov-aux@users.noreply.github.com>
2022-06-03 20:03:01 +0300

    Log request and response bodies

--- src/main/java/com/exchanger/exchange_api/aspect/ControllerAspect.java
+++ src/main/java/com/exchanger/exchange_api/aspect/ControllerAspect.java
@@ -1,7 +1,10 @@
 package com.exchanger.exchange_api.aspect;
 
+import com.exchanger.exchange_api.dto.ErrorResponseDTO;
 import com.exchanger.exchange_api.enumeration.ErrorCode;
 import com.exchanger.exchange_api.exception.HttpResponseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,8 +14,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.ConstraintViolationException;
 import java.lang.annotation.Annotation;
@@ -22,12 +29,15 @@ import java.lang.annotation.Annotation;
 @Aspect
 public class ControllerAspect {
     private final Logger logger = LoggerFactory.getLogger("API CALL");
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Pointcut("execution(* *.*(..)) && within(com.exchanger.exchange_api.controller.*)")
-    public void controllers() {}
+    public void controllers() {
+    }
 
     @Pointcut("within(@org.springframework.web.bind.annotation.RequestMapping *)")
-    public void requestMapping() {}
+    public void requestMapping() {
+    }
 
     @Around("controllers() && requestMapping()")
     public Object around(ProceedingJoinPoint joinPoint) {
@@ -35,29 +45,44 @@ public class ControllerAspect {
 
         try {
             Object o = joinPoint.proceed();
-            //todo Request body if any
-            //todo response code if any
-            new Thread(() -> logRequest(joinPoint, startTime)).start();
+            new Thread(() -> logRequest(joinPoint, startTime, (ResponseEntity<?>) o)).start();
             return o;
         } catch (Throwable e) {
+            ResponseEntity<ErrorResponseDTO> errorResponse;
             if (e instanceof HttpResponseException)
-                //todo log error
-                return ((HttpResponseException) e).getResponse();
-            if (e instanceof ConstraintViolationException ||
+                errorResponse = ((HttpResponseException) e).getResponse();
+            else if (e instanceof ConstraintViolationException ||
                     e instanceof MethodArgumentNotValidException) {
-                return new HttpResponseException(ErrorCode.VALIDATION_ERROR, e.getMessage()).getResponse();
+                errorResponse = new HttpResponseException(ErrorCode.VALIDATION_ERROR, e.getMessage()).getResponse();
+            } else {
+                e.printStackTrace();
+                errorResponse = new HttpResponseException(ErrorCode.INTERNAL_ERROR).getResponse();
             }
-            return new HttpResponseException(ErrorCode.INTERNAL_ERROR).getResponse();
+
+            new Thread(() -> logRequest(joinPoint, startTime, errorResponse)).start();
+            return errorResponse;
         }
     }
 
-    private void logRequest(ProceedingJoinPoint joinPoint, final long startTime) {
+    private void logRequest(ProceedingJoinPoint joinPoint, final long startTime, ResponseEntity<?> response) {
         final long timeTaken = System.currentTimeMillis() - startTime;
         final String method = joinPoint.getSignature().getName();
         final String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
         final String args = getArguments(joinPoint);
+        final int statusCode = response.getStatusCodeValue();
+        final Object requestBody = getRequestBody(joinPoint);
 
-        logger.info("{} ms | {}#{}({})", timeTaken, className, method, args);
+        if (requestBody != null) logBody(requestBody, "Request");
+        logger.info("{} | {}#{}({}) | {} ms ", statusCode, className, method, args, timeTaken);
+        if (statusCode != 204) logBody(response.getBody(), "Response");
+    }
+
+    private void logBody(Object request, String type) {
+        try {
+            final String body = objectMapper.writeValueAsString(request);
+            logger.info("{} Body: {}", type, body);
+        } catch (JsonProcessingException ignored) {
+        }
     }
 
     private String getArguments(ProceedingJoinPoint joinPoint) {
@@ -66,19 +91,44 @@ public class ControllerAspect {
         final String[] paramNames = methodSig.getParameterNames();
         final StringBuilder arguments = new StringBuilder();
         final Annotation[][] parameterAnnotations = methodSig.getMethod().getParameterAnnotations();
+        final StringBuilder queryParams = new StringBuilder("?");
 
         for (int i = 0; i < parameterAnnotations.length; i++) {
-            if (parameterAnnotations[i].length > 0 && parameterAnnotations[i][0] instanceof PathVariable)
+            if (parameterAnnotations[i].length == 0) continue;
+            if (parameterAnnotations[i][0] instanceof PathVariable)
                 arguments
                         .append(paramNames[i])
                         .append("=")
                         .append(args[i])
                         .append(", ");
+            else if (parameterAnnotations[i][0] instanceof RequestParam)
+                queryParams
+                        .append(paramNames[i])
+                        .append("=")
+                        .append(args[i])
+                        .append("&");
         }
 
         if (arguments.length() > 0)
             arguments.delete(arguments.length() - 2, arguments.length());
 
+        arguments.append(queryParams.delete(queryParams.length() - 1, queryParams.length()));
+
         return arguments.toString();
+    }
+
+    private Object getRequestBody(ProceedingJoinPoint joinPoint) {
+        final MethodSignature methodSig = (MethodSignature) joinPoint.getSignature();
+        if (!methodSig.getMethod().isAnnotationPresent(PostMapping.class)) return null;
+
+        final Annotation[][] parameterAnnotations = methodSig.getMethod().getParameterAnnotations();
+        final Object[] args = joinPoint.getArgs();
+
+        for (int i = 0; i < parameterAnnotations.length; i++) {
+            if (parameterAnnotations[i].length > 0 && parameterAnnotations[i][0] instanceof RequestBody) {
+                return args[i];
+            }
+        }
+        return null;
     }
 }
